205dc32444afa4242d94f6ed811e1f26b0c253db
OmarIdrisT <146827333+OmarIdrisT@users.noreply.github.com>
2023-11-07 22:37:12 +0100

    Update PracticaJS.js

--- PracticaJS.js
+++ PracticaJS.js
@@ -2,8 +2,10 @@ let numeroRandom = Math.floor(Math.random()*100000);
 let numeroUsuari = document.getElementById("numero");
 let numeroUsuariChain = numeroUsuari.split("");
 let numeroRandomChain = numeroRandom.split("");
+let 
 
 let tries = 0;
+let aciertos = 0;
 
 function numblejs() {
 
@@ -16,32 +18,49 @@ function numblejs() {
                 document.getElementById("intentos").appendChild(newdiv);
                 let mensaje = document.getElementById("mensaje");
 
-                if (numeroUsuariChain[i] == numeroRandomChain[i])
-                document.getElementsByClassName("newdiv").style.backg
-            }
-            tries++
+                newdiv.innerHTML = numeroUsuariChain[i]    
 
-            switch (tries) {
-                case 1: {
-                    mensaje.innerHTML = "Segundo intento, suerte!";
-                    break
+                if (numeroUsuariChain[i] == numeroRandomChain[i]) {
+                        document.getElementsByClassName("newdiv").style.backgroundColor = "green";
+                        aciertos++
                 }
-                case 2: {
-                    mensaje.innerHTML = "Tercer intento, suerte!";
-                    break
-                } 
-                case 3: {
-                    mensaje.innerHTML = "Cuidado, te quedan sólo 2 intentos.";
-                    break
+                else {
+                        for (let j = 0; j < 5, j++) {
+                                if (numeroUsuariChain[i] == numeroRandomChain[j]) {
+                                        document.getElementsByClassName("newdiv").style.backgroundColor = "yellow";
+                                }
+                        }
                 }
-                case 4: {
-                    mensaje.innerHTML = "Último intento...";
-                    break
-                }
-                case 5: {
-                    mensaje.innerHTML = "Ya no te quedan intentos...";
-                    break
-                }    
+            } 
+            tries++
+            document.getElementById("numero").value = "";    
+
+            if (aciertos == 5) {
+                    mensaje.innerHTML = "Has acertado!";
+                    document.getElementById("mensaje").style.backgroundColor = "green";
             }
-        }
+            else {    
+                    switch (tries) {
+                        case 1: {
+                            mensaje.innerHTML = "Segundo intento, suerte!";
+                            break
+                        }
+                        case 2: {
+                            mensaje.innerHTML = "Tercer intento, suerte!";
+                            break
+                        } 
+                        case 3: {
+                            mensaje.innerHTML = "Cuidado, te quedan sólo 2 intentos.";
+                            break
+                        }
+                        case 4: {
+                            mensaje.innerHTML = "Último intento...";
+                            break
+                        }
+                        case 5: {
+                            mensaje.innerHTML = "Ya no te quedan intentos...";
+                            break
+                        }    
+                    }
+         }
 }
