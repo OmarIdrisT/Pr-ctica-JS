5592eff3767de10b645467dbdfeb8d74b9f9a782
sjo <omar.idris.7e7@itb.cat>
2023-11-08 09:56:53 +0100

    wdwdw

--- PracticaJS.js
+++ PracticaJS.js
@@ -1,14 +1,18 @@
-let numeroRandom = Math.floor(Math.random()*100000);
+let numeroRandom = Math.floor((Math.random() * (99999 - 10000 + 1)) + 10000);
 let numeroRandomChain = numeroRandom.toString().split("");
-let mensaje = document.getElementById("mensaje");
 let tries = 0;
-let aciertos = 0;
+console.log(numeroRandom);
 
 function numblejs() {
 
+        let aciertos = 0;
+
         if (tries < 5) {
+
             let numeroUsuari = document.getElementById("numero").value;
+            numeroUsuari = numeroUsuari.padEnd  (5, "0");
             let numeroUsuariChain = numeroUsuari.split("");
+            console.log(numeroUsuari)
 
             for (let i = 0; i < 5; i++) {
 
@@ -29,37 +33,47 @@ function numblejs() {
                                 }
                         }
                 } 
-                switch (tries) {
-                    case 1: {
-                        mensaje.innerHTML = "Segundo intento, suerte!";
-                        break;
-                    }
-                    case 2: {
-                        mensaje.innerHTML = "Tercer intento, suerte!";
-                        break;
-                    } 
-                    case 3: {
-                        mensaje.innerHTML = "Cuidado, te quedan sólo 2 intentos.";
-                        break;
-                    }
-                    case 4: {
-                        mensaje.innerHTML = "Último intento...";
-                        break;
-                    }
-                    case 5: {
-                        mensaje.innerHTML = "Ya no te quedan intentos...";
-                        break;
-                    }    
-                }
-            } 
-        }
+            }
+        } 
         
         tries++;
-        document.getElementById("numero").value = "";    
+        document.getElementById("numero").value = "";  
 
         if (aciertos === 5) {
-                mensaje.innerHTML = "Has acertado!";
-                mensaje.style.backgroundColor = "green";
                 tries = 5;
+                document.getElementById("mensaje").innerHTML = "Has acertado!";
+                document.getElementById("mensaje").style.backgroundColor = "green";
+                let gifVictoria = document.createElement("img");
+                gifVictoria.src="penguinz0-yeah-baby.gif";
+                document.getElementById("gif").appendChild(gifVictoria);
+        }
+
+        else {
+            switch (tries) {
+                case 1: {
+                    document.getElementById("mensaje").innerHTML = "Segundo intento, suerte!";
+                    break;
+                }
+                case 2: {
+                    document.getElementById("mensaje").innerHTML = "Tercer intento, suerte!";
+                    break;
+                } 
+                case 3: {
+                    document.getElementById("mensaje").innerHTML = "Cuidado, te quedan sólo 2 intentos.";
+                    break;
+                }
+                case 4: {
+                    document.getElementById("mensaje").innerHTML = "Último intento...";
+                    break;
+                }
+                case 5: {
+                    document.getElementById("mensaje").innerHTML = "Ya no te quedan intentos...";
+                    let gifDerrota = document.createElement("img");
+                    gifDerrota.src="ataud.gif";
+                    document.getElementById("gif").appendChild(gifDerrota);
+                    break;
+                }  
+            }  
         }
 }
+
